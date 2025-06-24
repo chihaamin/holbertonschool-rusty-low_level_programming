937faf6e35ee9edc54ec9feb2193262da84891a4
chihaamin <amin.chiha@gmail.com>
2025-06-24 14:15:49 +0100

    Add implementation of 4-print_alphabt.rs to print lowercase alphabet excluding 'e' and 'q'

---
+++ variables_if_else_while/4-print_alphabt.rs
@@ -0,0 +1,29 @@
+use std::io::{self, Write};
+/**
+ * putchar - prints a character to standard output
+ * @c: the character to print
+ * This function writes a single character to the standard output
+ * and flushes the output buffer to ensure it appears immediately.
+ * Returns () nothing aka "void" in C.
+ */
+fn putchar(c: char) -> () {
+    let mut stdout = io::stdout();
+    stdout.write(&[c as u8]).unwrap();
+    stdout.flush().unwrap();
+}
+
+/**
+ * main - entry point of the program
+ * This function prints the lowercase alphabet from 'a' to 'z' except for 'e' and 'q',
+ * using the putchar function to output each character.
+ * followed by a newline character.
+ * Returns () nothing aka "void" in C.
+ */
+fn main() -> () {
+    for c in 'a'..='z' {
+        if c != 'e' && c != 'q' {
+            putchar(c);
+        }
+    }
+    putchar('\n');
+}
