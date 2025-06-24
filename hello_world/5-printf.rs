e2ad59038b6ef347fea5ceeb61873fd38ea9ffb6
chihaamin <amin.chiha@gmail.com>
2025-06-24 13:07:33 +0100

    Adding 5-printf.rs

---
+++ hello_world/5-printf.rs
@@ -0,0 +1,9 @@
+/*
+main - Entry point of the program
+This function writes a formatted string to the standard output using the print! macro.
+Returns: ()
+ */
+fn main() -> () {
+    // Using the `print!` macro to write a formatted string to standard output
+    print!("with proper grammar, but the outcome is a piece of art,\n");
+}
