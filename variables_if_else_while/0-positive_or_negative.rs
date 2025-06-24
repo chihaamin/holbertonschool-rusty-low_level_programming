946a9a1e1f3dd314f91319ddc9b577a1b711ba7b
chihaamin <amin.chiha@gmail.com>
2025-06-24 13:53:55 +0100

    Add .gitignore and initial implementation of positive_or_negative.rs

---
+++ variables_if_else_while/0-positive_or_negative.rs
@@ -0,0 +1,26 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+/**
+* main - Entry point of the program
+* This program generates a pseudo-random number based on the current time
+* and checks if it is positive, negative, or zero.
+* Returns: Nothing
+*/
+ */
+
+fn main() -> () {
+    let seed = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+
+    let n = (seed.wrapping_mul(1103515245) + 12345) as i32;
+
+    print!("{} ", n);
+    if n > 0 {
+        println!("is positive");
+    } else if n < 0 {
+        println!("is negative");
+    } else {
+        println!("is zero");
+    }
+}
